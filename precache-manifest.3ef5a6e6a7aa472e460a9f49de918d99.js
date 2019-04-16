637355313125fc05f0292c6976bb1229c2685e13
Stuart Clennett <stuartclennett@gmail.com>
2019-04-16 12:18:51 +0100

    Updates

---
+++ precache-manifest.3ef5a6e6a7aa472e460a9f49de918d99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a91cd7ac9ec82b0c0cf",
+    "url": "/stuartclennett/react-tasklist/static/js/runtime~main.2abfafc2.js"
+  },
+  {
+    "revision": "03e230cb52528644fdcc",
+    "url": "/stuartclennett/react-tasklist/static/js/main.2a542cf5.chunk.js"
+  },
+  {
+    "revision": "0f07640e69a83bd5f817",
+    "url": "/stuartclennett/react-tasklist/static/js/2.5487760d.chunk.js"
+  },
+  {
+    "revision": "03e230cb52528644fdcc",
+    "url": "/stuartclennett/react-tasklist/static/css/main.c908fd34.chunk.css"
+  },
+  {
+    "revision": "e92f12ae9a62d1e74e35df8b444b2c81",
+    "url": "/stuartclennett/react-tasklist/index.html"
+  }
+];
